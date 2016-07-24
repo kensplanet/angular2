5109a0fb38ceb4dc1b437152ae155f3bdae51055
kensplanet <kensplanet@yahoo.com>
2016-07-24 16:00:39 -0400

    New changes

--- src/app/first-app.component.ts
+++ src/app/first-app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {OtherComponent, AnotherComponent} from './other';
 
 @Component({
-  moduleId: module.id,
-  selector: 'first-app-app',
-  templateUrl: 'first-app.component.html',
-  styleUrls: ['first-app.component.css']
+    moduleId: module.id,
+    selector: 'first-app-app',
+    template: `
+    <h1>Inline Template</h1>
+    <fa-other></fa-other>
+    <fa-another></fa-another>
+   `,
+    styles: [`
+      h1 {
+        color: red;
+      }
+    `],
+    directives: [OtherComponent, AnotherComponent]
 })
 export class FirstAppAppComponent {
-  title = 'Does this show up?';
+    title = 'Does this show up?';
 }
