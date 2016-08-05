41e851f3c0c7e320df515af606ab13d347f87fd3
kensplanet <kensplanet@yahoo.com>
2016-08-04 21:39:25 -0400

    New changes

--- src/app/first-app.component.ts
+++ src/app/first-app.component.ts
@@ -1,20 +1,14 @@
 import {Component} from '@angular/core';
-import {OtherComponent, AnotherComponent} from './other';
+import { DatabindingComponent } from './databinding';
 
 @Component({
     moduleId: module.id,
     selector: 'first-app-app',
     template: `
-    <h1>Inline Template</h1>
-    <fa-other></fa-other>
-    <fa-another></fa-another>
+    <h1>Inline Template</h1>   
+     <app-databinding></app-databinding>
    `,
-    styles: [`
-      h1 {
-        color: red;
-      }
-    `],
-    directives: [OtherComponent, AnotherComponent]
+    directives:[DatabindingComponent]
 })
 export class FirstAppAppComponent {
     title = 'Does this show up?';
--- src/system-config.ts
+++ src/system-config.ts
@@ -30,6 +30,7 @@ const barrels: string[] = [
   'app',
   'app/shared',
   'app/other',
+  'app/databinding',
   /** @cli-barrel */
 ];
 
